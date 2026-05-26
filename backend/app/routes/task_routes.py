91dc2bb1e902dd7c1199eba5269bb1f5bd69bffe
yours-truly68 <mohammedrazim880@gmail.com>
2026-05-27 00:28:02 +0530

    Add endpoint to retrieve a single task with resource-level authorization

--- backend/app/routes/task_routes.py
+++ backend/app/routes/task_routes.py
@@ -4,6 +4,7 @@ from datetime import datetime
 from app.schemas.task_schema import TaskCreate
 from app.utils.dependencies import get_current_user
 from app.database.mongodb import task_collection
+from bson import ObjectId
 
 router = APIRouter()
 
@@ -45,3 +46,29 @@ def get_tasks(current_user: dict = Depends(get_current_user)):
         serialized_tasks.append(serialized_task)
 
     return {"tasks": serialized_tasks}
+
+
+@router.get("/tasks/{task_id}")
+def get_single_task(task_id: str, current_user: dict = Depends(get_current_user)):
+
+    # Resource Level Authorisation
+    task = task_collection.find_one(
+        {"_id": ObjectId(task_id), "user_id": str(current_user["_id"])}
+    )
+
+    if not task:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Task Not Found."
+        )
+
+    serialized_task = {
+        "_id": str(task["_id"]),
+        "title": task["title"],
+        "description": task["description"],
+        "status": task["status"],
+        "priority": task["priority"],
+        "created_at": task["created_at"],
+        "user_id": task["user_id"],
+    }
+
+    return {"task": serialized_task}
