e0eac11ce1206fd4157550f14648e204b8862eef
yours-truly68 <mohammedrazim880@gmail.com>
2026-05-27 15:53:58 +0530

    Wrap App component with AuthProvider for context management

--- frontend/task-manager-frontend/src/main.jsx
+++ frontend/task-manager-frontend/src/main.jsx
@@ -4,12 +4,15 @@ import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import Toaster from "react-hot-toast";
+import AuthProvider from "./context/AuthContext";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <Toaster position="top-right" reverseOrder={true} />
-      <App />
+      <AuthProvider>
+        <Toaster position="top-right" reverseOrder={true} />
+        <App />
+      </AuthProvider>
     </BrowserRouter>
   </StrictMode>,
 );
