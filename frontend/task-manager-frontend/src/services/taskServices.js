afc9d0c50a596dd1548716809d472b567c043aae
yours-truly68 <mohammedrazim880@gmail.com>
2026-05-27 17:00:13 +0530

    Add task management functions: deleteTask, getSingleTask, and updateTask

--- frontend/task-manager-frontend/src/services/taskServices.js
+++ frontend/task-manager-frontend/src/services/taskServices.js
@@ -11,3 +11,21 @@ export const createTask = async (taskData) => {
 
   return response.data;
 };
+
+export const deleteTask = async (taskId) => {
+  const response = await api.delete(`/tasks/${taskId}`);
+
+  return response.data;
+};
+
+export const getSingleTask = async (taskId) => {
+  const response = await api.get(`/tasks/${taskId}`);
+
+  return response.data;
+};
+
+export const updateTask = async (taskId, taskData) => {
+  const response = await api.put(`/tasks/${taskId}`, taskData);
+
+  return response.data;
+};
