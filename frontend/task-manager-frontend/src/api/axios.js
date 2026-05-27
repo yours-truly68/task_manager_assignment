b7923d57b0cd73b364e3dff804cf622cd433c08a
yours-truly68 <mohammedrazim880@gmail.com>
2026-05-27 17:10:26 +0530

    Update axios base URL to use environment variable for better configuration management

--- frontend/task-manager-frontend/src/api/axios.js
+++ frontend/task-manager-frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 //Interceptors intercept requests attach the Bearer token automatically while sending requests to the backend
