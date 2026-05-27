4d643b6b2e971093e3e4b721e3e66fb2a0288ca6
yours-truly68 <mohammedrazim880@gmail.com>
2026-05-27 15:54:05 +0530

    Add request interceptor to axios for automatic Bearer token attachment

--- frontend/task-manager-frontend/src/api/axios.js
+++ frontend/task-manager-frontend/src/api/axios.js
@@ -4,4 +4,20 @@ const api = axios.create({
   baseURL: "http://127.0.0.1:8000",
 });
 
+//Interceptors intercept requests attach the Bearer token automatically while sending requests to the backend
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
+
 export default api;
