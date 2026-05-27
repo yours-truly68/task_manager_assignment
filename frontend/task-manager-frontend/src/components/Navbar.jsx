022674488775c526e38c0c0e7e420a4e702c2faf
yours-truly68 <mohammedrazim880@gmail.com>
2026-05-27 15:54:38 +0530

    Implement Navbar component with logout functionality and navigation

--- frontend/task-manager-frontend/src/components/Navbar.jsx
+++ frontend/task-manager-frontend/src/components/Navbar.jsx
@@ -1,11 +1,31 @@
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
 
+import { AuthContext } from "../context/AuthContext";
 
-const Navbar = () =>{
-    return (
-        <nav>
-            <h1>Task manager</h1>
-        </nav>
-    )
+function Navbar() {
+  const { logout } = useContext(AuthContext);
+
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    logout();
+
+    navigate("/");
+  };
+
+  return (
+    <nav className="bg-black text-white px-8 py-4 flex justify-between items-center">
+      <h1 className="text-xl font-bold">Task Manager</h1>
+
+      <button
+        onClick={handleLogout}
+        className="bg-white text-black px-4 py-2 rounded-lg"
+      >
+        Logout
+      </button>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
