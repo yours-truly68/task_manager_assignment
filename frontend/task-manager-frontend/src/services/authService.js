03f47cf1cf4db8e030e84a258dcab47736be3571
yours-truly68 <mohammedrazim880@gmail.com>
2026-05-27 15:54:31 +0530

    Refactor authService to include loginUser function for user authentication

--- frontend/task-manager-frontend/src/services/authService.js
+++ frontend/task-manager-frontend/src/services/authService.js
@@ -5,3 +5,9 @@ export const registerUser = async (userData) => {
 
   return response.data;
 };
+
+export const loginUser = async (userData) => {
+  const response = await api.post("/auth/login", userData);
+
+  return response.data;
+};
