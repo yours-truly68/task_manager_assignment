54ab3a1e3279619baa7aaf709473f964873c2006
yours-truly68 <mohammedrazim880@gmail.com>
2026-05-27 20:04:01 +0530

    Add additional allowed origin to CORS middleware

--- backend/app/main.py
+++ backend/app/main.py
@@ -11,6 +11,7 @@ app.add_middleware(
     allow_origins=[
         "https://task-manager-assignment-3vq3-83ub51s0b.vercel.app",
         "http://localhost:5173",
+        "https://task-manager-assignment-5fml.vercel.app/",
     ],
     allow_credentials=True,
     allow_headers=["*"],
