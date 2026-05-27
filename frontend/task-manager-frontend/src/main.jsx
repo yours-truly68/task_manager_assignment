b96f023f0f32d19730ce4e9ea192d32304166a97
yours-truly68 <mohammedrazim880@gmail.com>
2026-05-27 17:01:49 +0530

    Fix import statement for Toaster component in main.jsx

--- frontend/task-manager-frontend/src/main.jsx
+++ frontend/task-manager-frontend/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
-import Toaster from "react-hot-toast";
+import { Toaster } from "react-hot-toast";
 import AuthProvider from "./context/AuthContext";
 
 createRoot(document.getElementById("root")).render(
